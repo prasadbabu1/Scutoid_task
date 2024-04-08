eb72e6b71c944d9652ac2e2856af0870b38daf4f
Prasad Babu <pbabu11088@gmail.com>
2024-04-08 17:38:52 +0530

    first commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import './index.css';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <main className={show ? 'space-toggle' : null}>
+      <header className={`header ${show ? 'space-toggle' : null}`}>
+        <div className='header-toggle' onClick={() => setShow(!show)}>
+          <i className={`fas fa-bars ${show ? 'fa-solid fa-xmark' : null}`}></i>
+        </div>
+        <div>
+        <i class="fa-solid fa-user" style={{color:"white",marginRight:"15px", cursor:"pointer"}}></i>
+        
+        <button type="button" class="btn btn-outline"> Registration</button>
+      </div>
+      </header>
+
+      <aside className={`sidebar ${show ? 'show' : null}`}>
+        <nav className='nav'>
+          <div>
+            <Link to='/' className='nav-logo'>
+              <i className={`fas fa-home-alt nav-logo-icon`}></i>
+              <span className='nav-logo-name'>Homepage</span>
+            </Link>
+
+            <div className='nav-list'>
+              <Link to='/products' className='nav-link'>
+              <i class="fa-solid fa-cart-shopping nav-link-icon"></i>
+                <span className='nav-link-name'>Products</span>
+              </Link>
+              <Link to='/guides' className='nav-link'>
+              <i class="fa-solid fa-file nav-link-icon"></i>               
+               <span className='nav-link-name'>Guides</span>
+              </Link>
+              <Link to='/gallery' className='nav-link'>
+                <i className='fas fa-image nav-link-icon'></i>
+                <span className='nav-link-name'>Gallery</span>
+              </Link>
+              <Link to='/transaction' className='nav-link'>
+                <i className='fas fa-dollar-sign nav-link-icon'></i>
+                <span className='nav-link-name'>Transaction</span>
+              </Link>
+
+
+              <div class="nav-link">
+
+              <i class="fa-solid fa-list"></i>
+                <div class="dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"  >
+                  More
+                </div>
+                <ul style={{backgroundColor:"#efadeb"}} class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li><a class="dropdown-item nav-link-name" href="#">WorkOrder</a></li>
+                  <li><a class="dropdown-item nav-link-name" href="#">Another action</a></li>
+                  <li><a class="dropdown-item nav-link-name" href="#">Something else here</a></li>
+                </ul>
+              </div>
+
+
+
+            </div>
+          </div>
+
+          <Link to='/logout' className='nav-link'>
+            <i className='fas fa-sign-out nav-link-icon'></i>
+            <span className='nav-link-name'>Logout</span>
+          </Link>
+        </nav>
+      </aside>
+      
+      <div class="row row-cols-1 row-cols-md-3 g-4" style={{marginTop:"15px"}}>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image1.avif" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image2.webp" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image3.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image4.avif" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image6.avif" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image7.avif" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image10.webp" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/image5.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="/images/617Y61foBsL._AC_UY1100_.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </main>
+  );
+};
+
+export default Sidebar;
